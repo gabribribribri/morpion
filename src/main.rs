ff89acc6873623a242a247cedaeba81f5a5f399f
gabribribribri <fuelanemo149@gmail.com>
2026-01-04 04:29:41 +0100

    implemented recursive backtracking

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ struct Morpion {
     turn: bool,
 }
 
+#[derive(Debug)]
 enum PlacementError {
     OutOfRange,
     CellAlreadyOccupied,
@@ -57,19 +58,41 @@ fn get_user_input() -> usize {
                 if idx == 0 || idx >= 10 {
                     continue;
                 } else {
-                    return idx-1;
+                    return idx - 1;
                 }
             }
             Err(_) => continue,
         }
     }
 }
+fn check_win(grid: [Option<bool>; 9]) -> Option<Option<bool>> {
+    for pattern in WIN_PATTERN {
+        if grid[pattern[0]] == grid[pattern[1]]
+            && grid[pattern[1]] == grid[pattern[2]]
+            && grid[pattern[0]].is_some()
+        {
+            return Some(grid[pattern[0]]);
+        }
+    }
 
+    if grid.iter().all(|c| c.is_some()) {
+        Some(None)
+    } else {
+        None
+    }
+}
+
+fn indexes_empty_cell(grid: [Option<bool>; 9]) -> impl std::iter::Iterator<Item = usize> {
+    grid.into_iter()
+        .enumerate()
+        .filter(|(_, c)| c.is_none())
+        .map(|(i, _)| i)
+}
 impl Morpion {
     fn new() -> Self {
         Morpion {
             grid: [None; 9],
-            turn: false,
+            turn: true,
         }
     }
 
@@ -86,28 +109,13 @@ impl Morpion {
         }
     }
 
-    fn check_win(&self) -> Option<Option<bool>> {
-        for pattern in WIN_PATTERN {
-            if self.grid[pattern[0]] == self.grid[pattern[1]]
-                && self.grid[pattern[1]] == self.grid[pattern[2]]
-                && self.grid[pattern[0]].is_some()
-            {
-                return Some(self.grid[pattern[0]]);
-            }
-        }
-
-        if self.grid.iter().all(|c| c.is_some()) {
-            Some(None)
-        } else {
-            None
-        }
-    }
-
     fn gameloop(&mut self) {
         loop {
-            println!("{}", self);
+            if self.turn {
+                println!("{}", self);
+            }
 
-            if let Some(end_entity) = self.check_win() {
+            if let Some(end_entity) = check_win(self.grid) {
                 match end_entity {
                     Some(player) => println!("Player {} wins !", if player { 'O' } else { 'X' }),
                     None => println!("Égalité !"),
@@ -115,20 +123,62 @@ impl Morpion {
                 return;
             }
 
-            loop {
-                match self.play_at(get_user_input()) {
-                    Ok(()) => (),
-                    Err(_) => continue,
-                }
-                break
+            match self.turn {
+                true => self.human_plays(),
+                false => self.bot_plays(),
             }
 
-            self.turn = !self.turn
+            self.turn = !self.turn;
         }
+    }
+
+    fn human_plays(&mut self) {
+        loop {
+            match self.play_at(get_user_input()) {
+                Ok(()) => (),
+                Err(_) => continue,
+            }
+            break;
+        }
+    }
+
+    fn bot_plays(&mut self) {
+        let idx_to_play = indexes_empty_cell(self.grid)
+            .map(|i| {
+                let mut next_grid = self.grid;
+                next_grid[i] = Some(false);
+                let wp = win_proba(next_grid, false);
+                println!(">> i={} -> p={}", i, wp);
+                (i, wp)
+            })
+            .max_by(|a, b| a.1.total_cmp(&b.1))
+            .unwrap()
+            .0;
+        println!(">>> will play at {}", idx_to_play);
+        self.play_at(idx_to_play).unwrap()
     }
 }
 
 fn main() {
     let mut mpn = Morpion::new();
     mpn.gameloop();
+}
+
+fn win_proba(grid: [Option<bool>; 9], turn: bool) -> f32 {
+    match check_win(grid) {
+        Some(Some(true)) | Some(None) => return 0.0,
+        Some(Some(false)) => return 1.0,
+        None => (),
+    }
+
+    let mut number_of_possible_move = 0.0;
+    let mut win_probability = 0.0;
+    for i in indexes_empty_cell(grid) {
+        let mut next_grid = grid;
+        next_grid[i] = Some(turn);
+        win_probability += win_proba(next_grid, !turn);
+        number_of_possible_move += 1.0
+    }
+
+    return win_probability / number_of_possible_move;
 }
